fa8327cad9980d0ada9908ddc853e84755ac9a85
Michael Sokol <mikaa123@gmail.com>
2012-12-04 17:53:22 +0100

    Removing old app.js file, not needed now that there's a bin.

--- app.js
+++
@@ -1,6 +0,0 @@
-var ansiHelper = require('./ansiHelper'),
-	fireplace = require('./fireplace');
-
-fireplace.burnLogs(15, function(fireString) {
-	console.log(ansiHelper.writeFrame(fireString));
-});
